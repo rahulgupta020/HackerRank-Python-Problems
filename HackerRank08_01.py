890980afc71c8b6c2e9f4765afb18b59edf4eb10
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2023-01-08 15:48:56 +0530

    Create HackerRank08_01.py

---
+++ HackerRank08_01.py
@@ -0,0 +1,11 @@
+"""
+Find the Runner-Up Score!
+
+Code:
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    result = sorted(set(arr))[-2]
+    print(result)
