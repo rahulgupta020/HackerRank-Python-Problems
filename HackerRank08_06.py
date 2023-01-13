1e13411f9b459d89c6f2c51f4417dcea4a827db3
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2023-01-13 10:07:26 +0530

    Create HackerRank08_06.py

---
+++ HackerRank08_06.py
@@ -0,0 +1,15 @@
+"""
+Find the Runner-Up Score!
+
+Code:
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    lst=[]
+    for i in arr:
+        if i not in lst:
+            lst.append(i)
+    lst.remove(max(lst))
+    print(max(lst))
